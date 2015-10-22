daad73fd5af71b4c872c26bf58cf833b5ab50360
Jenny Mangaly <jmangaly@gmail.com>
2015-10-22 09:22:20 +0000

    Setting up first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Moochster::Application.routes.draw do
+  root 'places#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
