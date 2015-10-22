4c49ef587d4bf3c0b6c04eb9b6412bf581bd2336
Jenny Mangaly <jmangaly@gmail.com>
2015-10-22 08:47:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moochster::Application.config.secret_key_base = 'dbb528e75e9528c3fcf2eb87da81af55570dd64567052864f43405876eccc84a5732fa574942d8c9906f5209a40600d19b8a017c70010b0f6991643949dcd1e6'
