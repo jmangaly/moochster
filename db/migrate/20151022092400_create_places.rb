bd0279de33c6dadc91bcd44cfdbb3677f100f19f
Jenny Mangaly <jmangaly@gmail.com>
2015-10-24 18:20:16 +0000

    Set up models and db places

---
+++ db/migrate/20151022092400_create_places.rb
@@ -0,0 +1,12 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+
+      t.string :name
+      t.text :description
+      t.string :address
+      	
+      t.timestamps
+    end
+  end
+end
