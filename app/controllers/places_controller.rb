dbe5c4cdc85d39165d8116f7dfdddb1038c2891a
Jenny Mangaly <jmangaly@gmail.com>
2015-11-02 17:17:21 +0000

    Allow edit and destroy of places only by proper users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
-  
+    before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+
   def index
     @places = Place.all
   end
@@ -20,18 +20,29 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+  end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
       @place = Place.find(params[:id])
-      @place.destroy
-      redirect_to root_path
+       if @place.user != current_user
+        return render :text => 'Not Allowed', :status => :forbidden
+  end
+
+    @place.destroy
+    redirect_to root_path
   end
 
   private
