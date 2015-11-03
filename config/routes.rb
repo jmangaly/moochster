a372cd96f3a7488bbe36cd1a12e40a30d6b0f953
Jenny Mangaly <jmangaly@gmail.com>
2015-11-03 17:24:39 +0000

    Add the submit button on comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Moochster::Application.routes.draw do
   devise_for :users
   root 'places#index'
 
+   resources :places do
+    resources :comments, :only => :create
+  end
+
   resources :places
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
