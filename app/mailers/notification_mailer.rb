fb7df07db1edc325b7e1b9b9da2d4135ad22dcc9
Jenny Mangaly <jmangaly@gmail.com>
2015-11-04 19:35:23 +0000

    Adjusting the automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@moochsterapp.com"
 
-  def comment_added
-  	 mail(to: "jmangaly@gmail.com",
-         subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	 @place = comment.place
+  	 @place_owner = @place.user
+  	 mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Moochster::Application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
