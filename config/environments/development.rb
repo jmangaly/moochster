14cc165edccbabc9c3dc05b063d6638b4201350f
Jenny Mangaly <jmangaly@gmail.com>
2015-11-04 17:30:04 +0000

    Previewing the automated email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,15 @@
 Moochster::Application.configure do
-  config.action_mailer.default_url_options = { :host => 'moochster-jm.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -16,7 +26,7 @@ Moochster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
