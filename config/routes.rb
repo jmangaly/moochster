67e3000507a614789f7b69f85879fb78d90878a1
Jenny Mangaly <jmangaly@gmail.com>
2015-11-05 19:24:59 +0000

    Building the User Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Moochster::Application.routes.draw do
     resources :comments, :only => :create
   end
 
-  resources :places
+  resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
